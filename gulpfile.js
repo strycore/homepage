385f5ba967a4281afa15212becd4a4732f8bfdde
Mathieu Comandon <strycore@gmail.com>
2016-06-27 20:46:15 -0700

    Setup gulp, bower, font awesome and skeleton framework

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+
+gulp.task('css', function() {
+  gulp.src([
+    'bower_components/skeleton-framework/dist/skeleton.min.css', 
+    'bower_components/font-awesome/css/font-awesome.min.css'
+  ])
+  .pipe(concat('styles.css'))
+  .pipe(gulp.dest('assets/css'));
+});
+
+
+gulp.task('fonts', function() {
+  gulp.src(['bower_components/font-awesome/fonts/*'])
+    .pipe(gulp.dest('assets/fonts'))
+});
+
+
+
+gulp.task('default', function(){
+  // run tasks here
+  // set up watch handlers here
+});
