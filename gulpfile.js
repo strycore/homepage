870680d3acb06d9f9f237784829f25c5f255efe4
Mathieu Comandon <strycore@gmail.com>
2016-06-28 12:10:34 -0700

    Setup template system with nunjucks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var browserSync = require('browser-sync').create();
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const nunjucks = require('gulp-nunjucks');
+const modRewrite  = require('connect-modrewrite');
 
 gulp.task('css', function() {
   gulp.src([
@@ -19,14 +21,26 @@ gulp.task('fonts', function() {
     .pipe(gulp.dest('assets/fonts'))
 });
 
-gulp.task('serve', ['css'], function() {
+gulp.task('html', function () {
+  gulp.src(['src/templates/*.html'])
+    .pipe(nunjucks.compile())
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('serve', ['html', 'css'], function() {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: "./dist",
+            middleware: [
+                modRewrite([
+                    '^.([^\\.]+)$ /$1.html [L]'
+                ])
+            ]
         }
     });
     gulp.watch('src/css/*.css', ['css']);
-    gulp.watch('*.html').on('change', browserSync.reload);
+    gulp.watch('src/templates/*.html', ['html']);
+    gulp.watch('dist/*.html').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['serve']);
