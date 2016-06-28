426bf68831ebbb1ff08875ed69c16529869c1a0f
Mathieu Comandon <strycore@gmail.com>
2016-06-28 09:31:44 -0700

    Setup BrowserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
-
+var browserSync = require('browser-sync').create();
 
 gulp.task('css', function() {
   gulp.src([
-    'bower_components/skeleton-framework/dist/skeleton.min.css', 
+    'bower_components/skeleton-framework/dist/skeleton.min.css',
     'bower_components/font-awesome/css/font-awesome.min.css',
     'src/css/main.css'
   ])
   .pipe(concat('styles.css'))
-  .pipe(gulp.dest('assets/css'));
+  .pipe(gulp.dest('assets/css'))
+  .pipe(browserSync.stream());
 });
 
 
@@ -18,9 +19,14 @@ gulp.task('fonts', function() {
     .pipe(gulp.dest('assets/fonts'))
 });
 
-
-
-gulp.task('default', function(){
-  // run tasks here
-  // set up watch handlers here
+gulp.task('serve', ['css'], function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch('src/css/*.css', ['css']);
+    gulp.watch('*.html').on('change', browserSync.reload);
 });
+
+gulp.task('default', ['serve']);
